184b5055b5bb6739e6030641754508bba115b3a5
satankita <ankitasatish@Ankitas-MacBook-Pro.local>
2023-05-30 18:13:58 -0700

    Deployed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5d7cfdc6ccmsh6b384063609f4a7p1299fbjsna801e13f1bc6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "47b8dd3cf485344aeb86a1d9b70b964a";
